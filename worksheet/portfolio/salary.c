28f7a91dbd1da59d9887db76014405f88da0c8d4
Onur106 <cmft0160@leeds.ac.uk>
2026-02-21 19:48:41 +0000

    end of week3

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,11 +3,47 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Onur Hasan Bakirhan
+ * ID: 201982434
  */
 
  int main( void ) {
+
+   double salary = 36250.0;
+   double ni_rate = 8.0;
+   double tax_rate = 15.0;
+   
+   double ni_contribution = 0.0;
+   double tax_contribution = 0.0;
+   double take_home_salary = 0.0;
+
+   double salary_after_ni = 0.0;
+   double taxable_income = 0.0;
+
+   double ni_fraction = ni_rate / 100.0;
+   double tax_fraction = tax_rate / 100.0;
+
+   ni_contribution = salary * ni_fraction;
+   salary_after_ni = salary - ni_contribution;
+
+   if (salary_after_ni > 12500.0)
+
+   {taxable_income = salary_after_ni - 12500.0;}
+
+   else 
+   {taxable_income = 0.0;}
+
+   tax_contribution = taxable_income * tax_fraction;
+
+   take_home_salary = salary_after_ni - tax_contribution;
+
+   printf("NI contribution £%.2f\n", ni_contribution);
+   printf("Tax contribution £%.2f\n", tax_contribution);
+   printf("Take home salary £%.2f\n", take_home_salary);
+
+    
+
+
 
     // define and initialise variables for the problem data 
 
