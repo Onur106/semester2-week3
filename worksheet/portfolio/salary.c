cfed8a5a8a2e054699e2f518384b77ad4332e219
Onur106 <cmft0160@leeds.ac.uk>
2026-02-21 20:11:03 +0000

    end of week3

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -45,15 +45,5 @@
 
 
 
-    // define and initialise variables for the problem data 
-
-    // calculate the deductions and final take-home salary
-
-    // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
-
     return 0;
  }
